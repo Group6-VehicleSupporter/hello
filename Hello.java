1872c7e7b404f020870f838a07f974eb6745fa6f
tamlth <thangrom112@gmail.com>
2016-06-04 09:50:22 +0700

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,9 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package hello;
+
+/**
+ *
+ * @author Tam
+ */
 public class Hello {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello tam");
+        Sholaship tam=new Sholaship();
+        System.out.println("Award: "+tam.getAward(10));
     }
-    
 }
